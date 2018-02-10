6b6bb510164bb61c55004771fe680293af36e196
Carl Mercier <carl@carlmercier.com>
2018-02-09 19:15:52 -0500

    fail when download or unzip fails

--- update_settings.sh
+++ update_settings.sh
@@ -4,10 +4,13 @@ DATETIME=`date '+%Y-%m-%d %H:%M:%S'`
 DATE=`date +%Y-%m-%d`
 
 cd "${0%/*}"
+git reset --hard
+git pull origin master
+
 rm PT-Files.zip
 rm -fr PT\ Files
-curl -o PT-Files.zip -L http://jasonappleton.com/Settings &> /dev/null
-unzip PT-Files.zip
+curl -o PT-Files.zip -L http://jasonappleton.com/Settings &> /dev/null || (echo "Can't download settings. Fail!"; exit 1)
+unzip PT-Files.zip || (echo "Can't unzip settings. Fail!"; exit 1)
 
 if [[ -z $(git status -s) || $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
   git add .
